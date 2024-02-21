4e22f76001741d2c20178180c009146d4fc7b6b2
yogniku <156093318+yogniku@users.noreply.github.com>
2024-02-21 22:33:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 //your JS code here. If required.
-// let para=document.querySelector('timer')
-// function currentTime(){
-// 	let date=new Date()
-// 	let hour=date.getHours()
-// 	let min= date.getMinutes()
-// 	let sec= date.getSeconds()
-// 	let day= date.getDay()
-// 	let month=date.getMonth()
-// 	let year=date.getFulyear()
-// 	let time=hour+":"+min+":"sec
-// 	para.innerText=`${month}/${day}/${year}/${time}`
-// return `${month}/${day}/${year}/${time}`
-// }
-// setInterval(currentTime,1000)
-// currentTime()
- function updateTimer() {
-            var timerElement = document.getElementById('timer');
-            var now = new Date();
-            var dateTimeString = now.toLocaleString();
-            timerElement.textContent = dateTimeString;
-        }
+let para=document.querySelector('timer')
+function currentTime(){
+	let date=new Date()
+	let hour=date.getHours()
+	let min= date.getMinutes()
+	let sec= date.getSeconds()
+	let day= date.getDay()
+	let month=date.getMonth()
+	let year=date.getFulyear()
+	let time=hour+":"+min+":"sec
+	para.innerText=`${month}/${day}/${year}/${time}`
+return `${month}/${day}/${year}/${time}`
+}
+setInterval(currentTime,1000)
+currentTime()
 
-        // Update the timer immediately and then every second
-        updateTimer();
-        setInterval(updateTimer, 1000);
+       
